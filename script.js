a16dd283535912666817df7239b0b71d6f470ecb
Unko <kobie.watkins@gmail.com>
2020-06-29 19:33:48 -0400

    edited some js

--- script.js
+++ script.js
@@ -14,38 +14,57 @@ $(document).ready(function () {
 
   let Title = "Die Hard";
   let Actor = "Bruce Willis";
-  let provider = ["Netflix", "Hulu", "AmazonPrimeVideo", "GooglePlay", "iTunes"];
+  let provider = [
+    "Netflix",
+    "Hulu",
+    "AmazonPrimeVideo",
+    "GooglePlay",
+    "iTunes",
+  ];
   let offerType = "";
   alert("are we talking");
 
   for (i = 0; i < provider.length; i++) {
-
-
     let settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/?PersonNames=" + Actor + "&Title=" + Title + "&ProgramTypes=Show&ProgramTypes=Movie&Providers=" + provider[i],
-      "method": "GET",
+      async: true,
+      crossDomain: true,
+      url:
+        "https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/?PersonNames=" +
+        Actor +
+        "&Title=" +
+        Title +
+        "&ProgramTypes=Show&ProgramTypes=Movie&Providers=" +
+        provider[i],
+      method: "GET",
       // error: checkContent(),
-      "headers": {
-        "x-rapidapi-host": "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com",
+      headers: {
+        "x-rapidapi-host":
+          "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com",
         "x-rapidapi-key": "acd626c871msh56e79dd581cd845p1e0ed3jsndbc38000616c",
-        "content-type": "application/json"
-      }
-
-    }
+        "content-type": "application/json",
+      },
+    };
     let service = provider[i];
     $.ajax(settings).done(function (response) {
-
-      if (service === "Netflix" || service === "Hulu" || service === "AmazonPrimeVideo") {
-
+      if (
+        service === "Netflix" ||
+        service === "Hulu" ||
+        service === "AmazonPrimeVideo"
+      ) {
         offerType = "Subscription";
       } else {
         offerType = "Buy or Rent";
       }
       console.log(response);
 
-      console.log("The are content provider requires:", offerType, "for their service", service, "has this much content matching your search available: ", response.Hits.length);
+      console.log(
+        "The are content provider requires:",
+        offerType,
+        "for their service",
+        service,
+        "has this much content matching your search available: ",
+        response.Hits.length
+      );
       let content = response.Hits.length - 1;
       for (c = 0; c < content; c++) {
         console.log("Titles: ", response.Hits[c].Source.Title);
@@ -53,18 +72,13 @@ $(document).ready(function () {
         console.log("status: ", response.Hits[c].Source.ProgramType);
         // console.log("status: ", response.Hits[c].Source.OfficialSiteUrl);
       }
-
-    })
-
-
-
+    });
   }
 
   // This .on("click") function will trigger the AJAX Call
-  $("#submit").click(function () {
-    const searchInput = JSON.parse(localStorage.getItem("Input")) || [];
-
+  $("#submit").on("click", function () {
+    // const searchInput = JSON.parse(localStorage.setItem("Input")) || [];
     // grab the text from the input box
-    var searchInput = movieSearch.text().val().trim;
+    // var searchInput = movieSearch.val().trim();
   });
 });
