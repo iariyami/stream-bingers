aa77f3f5456ca134c29e62eb371d5e93c1651a39
Unko <kobie.watkins@gmail.com>
2020-06-29 17:52:17 -0400

    new js code for project one

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let actor;
-let actress;
-let movies;
-let title;
-let cast;
-let rating;
-let onClick;
-let getYear;
+$(document).ready(function () {
+  //variable api key
+  let apiKey = "";
+
+  let actor = "";
+  let movies = "";
+  let title = "";
+  let cast = "";
+  let criticsRating = 0;
+  let getYear = "";
+  let movieRated = "";
+  var movieSearch = $(".Input");
+  var actorSearch = $(".Actor");
+
+  // This .on("click") function will trigger the AJAX Call
+  $("#submit").click(function () {
+    const searchInput = JSON.parse(localStorage.getItem("Input")) || [];
+
+    // grab the text from the input box
+    var searchInput = movieSearch.text().val().trim;
+  });
+});
