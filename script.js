f3535493d1a25a044f41162b1781743efba0f57f
victorsc45 <victorlitzau@gmail.com>
2020-07-02 12:53:30 -0400

    final push for presentation with Bulma responsiveness intact

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
   // clear media object for a clean UI
   $(".media").hide();
   $(".modal").hide();
@@ -16,8 +18,6 @@ $(document).ready(function () {
 
   // variables declared
   let offerType = "";
-
-
 
   // search API for OMDB and  re-renders the HTML to display the appropriate content
 
@@ -55,20 +55,29 @@ $(document).ready(function () {
       var imgURL = responseOMDB.Poster;
 
       // Creating an element to hold the image
-      let pImg = $("<p class='image is-200x200'>");
+      let pImg = $("<p class='image is-100x100  is-hidden-touch'>");
       let image = $("<img>").attr("src", imgURL);
       $(".media-left").append(pImg)
       // append poster image to media object dynamically
       pImg.append(image);
       // Transfer content to HTML and append to appropriate DOM element
+      // assign Bulma classes for responsiveness
       let p1 = $("<p>").text("Title:  " + Title).css("font-size", "25px");
+      p1.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
       let p2 = $("<p>").text("Year of Release:  " + getYear);
+      p2.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
       let p3 = $("<p>").text("Cast:  " + pCast);
+      p3.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
       let p4 = $("<p>").text("Short Plot:  " + pPlot);
+      p4.addClass("is-block-desktop-only is-hidden-touch");
       let p5 = $("<p>").text("Rated:  " + pRated);
+      p5.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
       let p6 = $("<p>").text("Critics Review:  " + criticsRatingS + " " + "gives it a " + criticsRatingV);
-      let spacer = $("<div>").css("margin-bottom", "200px");
+      p6.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
+      let spacer = $("<div>").css("margin-bottom", "250px");
+      spacer.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
       let hardhr = $("<hr>");
+      hardhr.addClass("is-block-desktop-only is-block-touch is-block-mobile");
       // show media content that was hidden for a clean looking UI
 
       $(".media").show();
@@ -130,14 +139,21 @@ $(document).ready(function () {
           contenType = response.Hits[c].Source.ProgramType;
 
           // Transfer content to HTML and append to appropriate DOM element
+          // assign Bulma classes for responsiveness
           let brline = $("<br>");
+          brline.addClass("is-block-desktop-only is-block-touch is-block-mobile");
           let separator = $("<hr>");
+          separator.addClass("is-block-desktop-only is-block-touch is-block-mobile");
           let pfive = $("<p>").text("The content provider " + service + " requires: " +
             offerType +
             " for their content and they have " + response.Hits.length + " related content to your search").css("font-weight", "300");
+          pfive.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
           let pOne = $("<p>").text("Titles:  " + relateTitles).css("font-size", "25px");
+          pOne.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
           let pTwo = $("<p>").text("The status of this content is: " + statusTitles);
+          pTwo.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
           let pThree = $("<p>").text("The type of this content is: " + contenType);
+          pThree.addClass("is-block-desktop-only is-block-touch is-inline-mobile");
           // Transfer content to HTML and append to appropriate DOM element
           $(".content").append(separator, pfive, brline, pOne, brline, pTwo, brline, pThree, separator);
         }
